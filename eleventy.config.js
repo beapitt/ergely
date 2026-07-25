dddf4d0d5a6d832c53c3cd613de885469621b4d6
Gaia Regino <gaiaregino@Gaias-MacBook-Pro-2.local>
2026-07-25 20:42:28 +0200

    fix static files passthrough

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("hero.webp");
+  eleventyConfig.addPassthroughCopy("index.html");
   return {
     dir: {
       input: ".",
